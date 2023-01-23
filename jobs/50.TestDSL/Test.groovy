c2f1f229896c251deeeeb0c1c9d3272098dc5c51
AndreiBaitau <87367249+AndreiBaitau@users.noreply.github.com>
2023-01-23 16:39:34 +0300

    Update Test.groovy

--- jobs/50.TestDSL/Test.groovy
+++ jobs/50.TestDSL/Test.groovy
@@ -1,5 +1,29 @@
-job('Hello World') {
-  steps {
-    shell('echo "Hello World!"')
-  }
-}
+pipeline {
+    agent any
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr:'5'))
+    }
+    stages {
+        stage('Clone repository') {
+            steps {
+                git url: 'https://github.com/AndreiBaitau/jenkins.git', branch: 'master'
+            }
+        }
+        stage('Checking repository'){
+            steps {
+                sh "ls -l"
+                sh "pwd"
+                sh "rm -rf /var/lib/jenkins/jobs/*0.*"
+            }
+        }
+        stage('Moving jobs from git to jenkins_home'){
+            steps{
+                sh "pwd"
+                sh "ls -l /var/lib/jenkins/jobs/"
+                sh "mv /var/lib/jenkins/workspace/LetsTry/jobs/* '/var/lib/jenkins/jobs/'"
+                sh "ls -l /var/lib/jenkins/jobs/"
+            }
+        }
+        }
+    }
